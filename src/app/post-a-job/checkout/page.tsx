1a86778c1c5439f8c7ee4c85d369173dddee78d3
jccy2106 <jccy2106@gmail.com>
2024-08-23 09:27:58 +0800

    stripe webhook

--- src/app/post-a-job/checkout/page.tsx
+++ src/app/post-a-job/checkout/page.tsx
@@ -1,22 +1,21 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
-import { loadStripe } from "@stripe/stripe-js";
+import {
+  useStripe,
+  useElements,
+  PaymentElement,
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { stripePromise } from '@/service/stripe';
 
 interface PageProps {
-    amount: number;
+  amount: number;
 }
 export default function Page(/*{ amount }: PageProps*/) {
-    const amount = 49.99;
+  const amount = 49.99;
 
-    const [errorMessage, setErrorMessage] = useState<string>();
-    const [clientSecret, setClientSecret] = useState('');
-    const [loading, setLoading] = useState(false);
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 18f744f (stripe integration WIP)
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [clientSecret, setClientSecret] = useState('');
+  const [loading, setLoading] = useState(false);
 }
