4589efaff3cc0fb7244d7ef2cbe322961e8a0fcd
ollehmichael <byongcheolhan@gmail.com>
2024-09-10 21:01:57 +0800

    chore: handle error on course page fetch

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -3,7 +3,18 @@ import { ResourceMap } from '@/constants/ContentList';
 import { sanityClient } from '@/service/sanity/client';
 
 export default async function Page() {
-  const posts = await sanityClient.fetch(ResourceMap.courses.query);
+  let posts;
+  try {
+    const response = await sanityClient.fetch(ResourceMap.courses.query);
+    posts = JSON.parse(JSON.stringify(response));
+  } catch (error) {
+    console.error('Error fetching or parsing courses:', error);
+    console.log(
+      'Raw response:',
+      await sanityClient.fetch(ResourceMap.courses.query)
+    );
+    posts = []; // Fallback to empty array if there's an error
+  }
   const header = ResourceMap.courses.header;
 
   return <ContentIndex header={header} content={posts} />;
